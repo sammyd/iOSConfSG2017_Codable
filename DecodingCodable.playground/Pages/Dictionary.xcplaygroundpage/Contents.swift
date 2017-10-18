9be5bf7277097a599f1d9b88759958008a7d220c
Sam Davies <sam@razeware.com>
2017-10-18 16:21:54 +0800

    Phase 2: Encoding/decoding a dictionary

--- DecodingCodable.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
+++ DecodingCodable.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
@@ -9,10 +9,12 @@ let dict = [
 ]
 
 let encoder = JSONEncoder()
-// TODO
+let encoded = try! encoder.encode(dict)
+
+let jsonString = String(bytes: encoded, encoding: .utf8)
 
 let decoder = JSONDecoder()
-// TODO
+let decoded = try! decoder.decode([String : Int].self, from: encoded)
 
 
 //: [Next](@next)
