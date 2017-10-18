3fe337b9216d0ed135ae15538e10e2a2e74da59d
Sam Davies <sam@razeware.com>
2017-10-18 15:56:54 +0800

    Handling deserialisation of the JSON

--- DecodingCodable.playground/Pages/Decoding.xcplaygroundpage/Contents.swift
+++ DecodingCodable.playground/Pages/Decoding.xcplaygroundpage/Contents.swift
@@ -7,11 +7,25 @@
 import Foundation
 
 struct Product: Codable {
+  private enum CodingKeys: String, CodingKey {
+    case id
+    case name
+    case image_url = "cover_url"
+    case url = "relative_url"
+    case sku
+  }
+  
   let id: Int
   let name: String
-  let image_url: URL
-  let url: URL
+  let image_url: URL?
+  let url: URL?
   let sku: String
+}
+
+extension Product: CustomPlaygroundQuickLookable {
+  var customPlaygroundQuickLook: PlaygroundQuickLook {
+    return .text("\(sku) :: \(name)")
+  }
 }
 
 guard let jsonUrl = Bundle.main.url(forResource: "products", withExtension: "json") else { fatalError() }
