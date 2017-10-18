0ae77f5a882c0f6f7890a3381bf968a36dd99fe7
Sam Davies <sam@razeware.com>
2017-10-18 11:45:30 +0800

    Phase 2: Encoding a dictionary

--- DecodingCodable.playground/Contents.swift
+++ DecodingCodable.playground/Contents.swift
@@ -1,12 +1,16 @@
 import UIKit
 
-let str = "A string"
+let dict = [
+  "people": 24,
+  "cats": 6,
+  "cars": 16
+]
 
 let encoder = JSONEncoder()
-let data = try! encoder.encode([str])
+let data = try! encoder.encode(dict)
 
 let jsonString = String(bytes: data, encoding: .utf8)
 
 let decoder = JSONDecoder()
-try! decoder.decode([String].self, from: data)
+try! decoder.decode([String : Int].self, from: data)
 
