ebabe52a22c84b79ef4d40f06a153de4ca30b885
Sam Davies <sam@razeware.com>
2017-10-18 15:12:25 +0800

    Adding a starter state for the decoding page

--- DecodingCodable.playground/Pages/Decoding.xcplaygroundpage/Contents.swift
+++ DecodingCodable.playground/Pages/Decoding.xcplaygroundpage/Contents.swift
@@ -6,4 +6,19 @@
 
 import Foundation
 
-var str = "Hello, playground"
+struct Product: Codable {
+  let id: Int
+  let name: String
+  let image_url: URL
+  let url: URL
+  let sku: String
+}
+
+guard let jsonUrl = Bundle.main.url(forResource: "products", withExtension: "json") else { fatalError() }
+
+do {
+  
+} catch let error {
+  print("There was a problem: \(error)")
+}
+
