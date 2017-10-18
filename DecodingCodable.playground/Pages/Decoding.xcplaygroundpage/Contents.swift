42b63146fe22ead966ba32c93b1a13fcfea0b1dc
Sam Davies <sam@razeware.com>
2017-10-18 16:03:30 +0800

    Nearly the starter project.

--- DecodingCodable.playground/Pages/Decoding.xcplaygroundpage/Contents.swift
+++ DecodingCodable.playground/Pages/Decoding.xcplaygroundpage/Contents.swift
@@ -7,14 +7,6 @@
 import Foundation
 
 struct Product: Codable {
-  private enum CodingKeys: String, CodingKey {
-    case id
-    case name
-    case image_url = "cover_url"
-    case url = "relative_url"
-    case sku
-  }
-  
   let id: Int
   let name: String
   let image_url: URL?
@@ -31,10 +23,7 @@ extension Product: CustomPlaygroundQuickLookable {
 guard let jsonUrl = Bundle.main.url(forResource: "products", withExtension: "json") else { fatalError() }
 
 do {
-  let jsonData = try Data(contentsOf: jsonUrl)
-  let decoder = JSONDecoder()
-  let products = try decoder.decode([Product].self, from: jsonData)
-  print(products)
+  // TODO
 } catch let error {
   print("There was a problem: \(error)")
 }
--- DecodingCodable.playground/Pages/Encoding.xcplaygroundpage/Contents.swift
+++ DecodingCodable.playground/Pages/Encoding.xcplaygroundpage/Contents.swift
@@ -2,44 +2,7 @@
 
 import UIKit
 
-enum Relation: String, Codable {
-  case mum
-  case dad
-}
-
-struct Colour: Codable {
-  let red, green, blue, alpha: CGFloat
-  init(from colour: UIColor) {
-    var cmpts : (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) = (0, 0, 0, 0)
-    colour.getRed(&cmpts.r, green: &cmpts.g, blue: &cmpts.b, alpha: &cmpts.a)
-    red = cmpts.r
-    green = cmpts.g
-    blue = cmpts.b
-    alpha = cmpts.a
-  }
-  
-  var uiColor: UIColor {
-    return UIColor(red: red, green: green, blue: blue, alpha: alpha)
-  }
-}
-
-struct Person: Codable {
-  let name: String
-  let age: Int
-  let favouriteColour: Colour
-  let relations: [Relation: String]
-}
-
-let julie = Person(name: "julie", age: 43, favouriteColour: Colour(from: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)), relations: [.mum: "Lucy", .dad: "Peter"])
-
-let encoder = JSONEncoder()
-let data = try! encoder.encode(julie)
-
-let jsonString = String(bytes: data, encoding: .utf8)
-
-let decoder = JSONDecoder()
-let decodedPerson = try! decoder.decode(Person.self, from: data)
-decodedPerson.favouriteColour.uiColor
+// TODO
 
 
 //: ---
