a1684508cbaf4750f66b26e894fe45dda9595aad
Sam Davies <sam@razeware.com>
2017-10-18 13:09:41 +0800

    Phase 5: Adding an enum

--- DecodingCodable.playground/Contents.swift
+++ DecodingCodable.playground/Contents.swift
@@ -1,13 +1,18 @@
 import UIKit
 
+enum Relation: Int, Codable {
+  case mum
+  case dad
+}
+
 struct Person: Codable {
   let name: String
   let age: Int
   let favouriteColour: String
-  let relations: [String: String]
+  let relations: [Relation: String]
 }
 
-let julie = Person(name: "julie", age: 43, favouriteColour: "orange", relations: ["mum": "Lucy", "dad": "Peter"])
+let julie = Person(name: "julie", age: 43, favouriteColour: "orange", relations: [.mum: "Lucy", .dad: "Peter"])
 
 let encoder = JSONEncoder()
 let data = try! encoder.encode(julie)
