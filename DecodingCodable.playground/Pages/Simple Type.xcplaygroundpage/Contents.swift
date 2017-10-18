f3ae45d6a8142ee3fd16d055f80ec2b21e71dad1
Sam Davies <sam@razeware.com>
2017-10-18 16:20:22 +0800

    Phase 1: Encoding/decoding a simple type

--- DecodingCodable.playground/Pages/Simple Type.xcplaygroundpage/Contents.swift
+++ DecodingCodable.playground/Pages/Simple Type.xcplaygroundpage/Contents.swift
@@ -2,7 +2,15 @@
 
 import UIKit
 
-// TODO
+let str = "My String"
+
+let encoder = JSONEncoder()
+let jsonData = try! encoder.encode([str])
+
+let jsonString = String(bytes: jsonData, encoding: .utf8)
+
+let decoder = JSONDecoder()
+let decoded = try! decoder.decode([String].self, from: jsonData)
 
 
 //: ---
